1bb9ab61b5294b9b842467bf80f8e3e4b2955724
Akash  Kumar Benjara <79560289+AKash-Kumar-Benjara@users.noreply.github.com>
2021-06-09 19:23:57 +0530

    Checkpoint 2

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Header from './common/header/Header'
+
+ReactDOM.render(
+  <><Header/></>,
+  document.getElementById('root')
+);
